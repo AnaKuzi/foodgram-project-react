6fa499cdc28f572236482feb355fd339da3a621f
Anastasia Kuzina <anastasia.kuzina88@gmail.com>
2023-01-28 13:26:38 +0300

    deploy test

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'secret_key')
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['158.160.32.176', '127.0.0.1', 'localhost']
 
