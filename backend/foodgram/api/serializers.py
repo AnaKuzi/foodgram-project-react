246170d990879ec41f99f0c60b041918db0e7c8c
Anastasia Kuzina <anastasia.kuzina88@gmail.com>
2023-01-30 08:00:52 +0300

    validation tests

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -194,21 +194,21 @@ class RecipeSerializer(serializers.ModelSerializer):
             )
         return data
 
-    def validate_ingredients(self, data):
-        ingredients = []
-        for ingredient in data:
-            if ingredient.get('amount').isnumeric() is False:
-                raise exceptions.ParseError(
-                    'Количество ингредиента должно быть целым числом')
-            else:
-                if int(ingredient.get('amount')) < 1:
-                    raise exceptions.ParseError(
-                        'Количество ингредиента должно быть больше нуля')
-                if ingredient.get('id') in ingredients:
-                    raise exceptions.ParseError(
-                        'Нельзя дублировать один ингридиент')
-                ingredients.append(ingredient.get('id'))
-        return data
+    # def validate_ingredients(self, data):
+    #     ingredients = []
+    #     for ingredient in data:
+    #         if ingredient.get('amount').isnumeric() is False:
+    #             raise exceptions.ParseError(
+    #                 'Количество ингредиента должно быть целым числом')
+    #         else:
+    #             if int(ingredient.get('amount')) < 1:
+    #                 raise exceptions.ParseError(
+    #                     'Количество ингредиента должно быть больше нуля')
+    #             if ingredient.get('id') in ingredients:
+    #                 raise exceptions.ParseError(
+    #                     'Нельзя дублировать один ингридиент')
+    #             ingredients.append(ingredient.get('id'))
+    #     return data
 
     def create(self, validated_data):
         tags = validated_data.pop('tags')
