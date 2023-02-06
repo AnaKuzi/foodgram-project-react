3b4e037df0c0f8f4b619d148b21166209d513155
Anastasia Kuzina <anastasia.kuzina88@gmail.com>
2023-02-06 18:50:09 +0300

    SK

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = 'd12(_k#-p#o%3zp7*(qst$_)*mz-)p8avfc6s-7bjp^=kvtp*v'
+SECRET_KEY = os.getenv('SECRET_KEY', 'secret_key')
 
 DEBUG = False
 
